d84fc64e3f20da0879ca24ac222de29335516a82
yngnicole <nicoley5@uci.edu>
2026-01-13 18:58:12 -0800

    s

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField]
+    // collider
+    // transform
+
     // Start is called before the first frame update
     void Start()
     {
